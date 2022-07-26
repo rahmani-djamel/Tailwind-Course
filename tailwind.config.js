231027e8625915caa0fb7f715e99c0967c692eba
rahmani djamel <hxhdono11@gmail.com>
2022-07-26 17:27:40 +0200

    navbar

--- tailwind.config.js
+++ tailwind.config.js
@@ -4,7 +4,27 @@ module.exports = {
     "./public/*.html"
   ],
   theme: {
-    extend: {},
+    extend: {
+      container: {
+        center: true,
+        padding: '1rem',
+      },
+      screens:{
+        "lg": "1124",
+        "xl": "1124",
+        "2xl": "1124"
+      },
+      colors: {
+        "bookmark-purple": "#5267DF",
+        "bookmark-red": "#FA5959",
+        "bookmark-blue": "#242A45",
+        "bookmark-grey": "#9194A2",
+        "bookmark-white": "#f7f7f7",
+      },
+    },
+    fontFamily:{
+      Poppins:["Poppins","sans-serif"]
+     }
   },
   plugins: [],
 }
